c9feb1fc629b9fa7ab17b4da6ff8c74c886f4c39
HoangQuocLuat <2021050769@student.humg.edu.vn>
2023-03-02 15:11:24 +0700

    ..

--- client/src/Router.js
+++ client/src/Router.js
@@ -1,8 +1,9 @@
 import Home from "./pages/Home";
-
+import Topic from "./pages/Topic"
 const publicRoutes = [
   { path: "/", component: Home },
   { path: "/home", component: Home },
+  { path: "/topic", component: Topic}
 ];
 
 const privateRoutes = [];
