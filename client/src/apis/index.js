d3a4f8ad2e26fc331a55b8f5ead209a1728485f6
ThoPham02 <2021050819@student.humg.edu.vn>
2023-03-30 09:09:51 +0700

    change client base url

--- client/src/apis/index.js
+++ client/src/apis/index.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-    baseURL: "http://localhost:8080",
+    baseURL: "https://nckh-be.onrender.com",
     headers: {
         Accept: "application/json",
         'Content-Type': "application/json"
