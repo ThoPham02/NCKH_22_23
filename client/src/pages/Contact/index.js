bb6ef92f2478c9c4399f9b1abd247dd52df1f0ce
HoangQuocLuat <2021050769@student.humg.edu.vn>
2023-04-13 15:02:35 +0700

    ...

---
+++ client/src/pages/Contact/index.js
@@ -0,0 +1,21 @@
+import "./contact.css"
+const Contact = () => {
+    return (
+        <div className="contact">
+            <p>Phòng hành chính</p>
+            <ul>
+                <li>
+                    <p>Địa chỉ: P307, nhà C 12 tầng</p>
+                </li>
+                <li>
+                    <p>Điện thoại: 02438386437</p>
+                </li>
+                <li>
+                    <p>Email: khoahoccongnghe@humg.edu.vn</p>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Contact
