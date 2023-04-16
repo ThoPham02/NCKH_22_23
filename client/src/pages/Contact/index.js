eec64174a46a9e4c87c4874e1a835b302c68aad6
HoangQuocLuat <2021050769@student.humg.edu.vn>
2023-04-16 15:20:07 +0700

    push code home

--- client/src/pages/Contact/index.js
+++ client/src/pages/Contact/index.js
@@ -1,19 +1,24 @@
 import "./contact.css"
 const Contact = () => {
     return (
-        <div className="contact">
-            <p>Phòng hành chính</p>
-            <ul>
-                <li>
-                    <p>Địa chỉ: P307, nhà C 12 tầng</p>
-                </li>
-                <li>
-                    <p>Điện thoại: 02438386437</p>
-                </li>
-                <li>
-                    <p>Email: khoahoccongnghe@humg.edu.vn</p>
-                </li>
-            </ul>
+        <div>
+            <div>
+                <p className="name__head">Liên hệ</p>
+            </div>
+            <div className="contact">
+                <p>Phòng hành chính</p>
+                <ul>
+                    <li>
+                        <p>Địa chỉ: P307, nhà C 12 tầng</p>
+                    </li>
+                    <li>
+                        <p>Điện thoại: 02438386437</p>
+                    </li>
+                    <li>
+                        <p>Email: khoahoccongnghe@humg.edu.vn</p>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
