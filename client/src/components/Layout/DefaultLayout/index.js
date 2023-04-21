b7d47b9cf1518de5a099fec55c59098892fd4f7b
ThoPham02 <2021050819@student.humg.edu.vn>
2023-04-21 22:24:20 +0700

    add fe

--- client/src/components/Layout/DefaultLayout/index.js
+++ client/src/components/Layout/DefaultLayout/index.js
@@ -2,16 +2,21 @@ import Header from "./Header";
 import Footer from "./Footer";
 import NavBar from "./Navbar";
 
+const background = require("./background.png")
+
 const DefaultLayout = ({ children }) => {
   return (
-    <>
+    <div style={{backgroundImage: `url(${background})`, backgroundSize: "cover"}}>
       <Header />
       <NavBar />
-      <div style={{ minHeight: "calc(100vh - 212px)", paddingTop: "4px" }} className="container">
+      <div className="container" style={{ 
+        minHeight: "calc(100vh - 219px)",
+        marginTop: "4px",
+      }}>
         {children}
       </div>
       <Footer />
-    </>
+    </ div>
   );
 };
 
