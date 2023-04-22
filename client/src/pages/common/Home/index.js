d229145e06aed6c4bfbe234fa39db5bc8dcb11f2
ThoPham02 <2021050819@student.humg.edu.vn>
2023-04-22 21:02:33 +0700

    add home

--- client/src/pages/common/Home/index.js
+++ client/src/pages/common/Home/index.js
@@ -1,7 +1,58 @@
-import "./style.css"
+import { MdOutlineKeyboardDoubleArrowRight } from "react-icons/md";
+import { useEffect, useState } from "react";
+
+import Card from "../../../components/Shares/Card";
+import { image1, image2, image3 } from "./imgHome";
+import { listNotifications, instructions } from "./data";
+import "./style.css";
 
 const Home = () => {
+  const images = [image1, image2, image3];
+  const [curent, setCurent] = useState(0);
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setCurent((curent + 1) % images.length);
+    }, 4000);
+    return () => clearInterval(intervalId);
+  });
+  return (
+    <div className="home">
+      <Card title="Thông Báo">
+        <div  className="notification">
+          <div className="notification-img">
+            <img src={images[curent]} alt="..." />
+          </div>
+          <div className="notification-content">
+            {listNotifications.map((notification, index) => {
+              return (
+                <div>
+                    <MdOutlineKeyboardDoubleArrowRight />
+                  <a key={index} href={notification.url}>
+                    {notification.name}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      </Card>
 
-}
+      <Card title="Hướng dẫn">
+      <div className="notification-content">
+            {instructions.map((notification, index) => {
+              return (
+                <div>
+                    <MdOutlineKeyboardDoubleArrowRight />
+                  <a key={index} href={notification.url}>
+                    {notification.name}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+      </Card>
+    </div>
+  );
+};
 
 export default Home;
