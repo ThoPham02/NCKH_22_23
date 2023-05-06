8613c6a3e6ea466f411257b40b81903333c8ed2c
ThoPham02 <2021050819@student.humg.edu.vn>
2023-05-06 09:48:05 +0700

    add action button

--- client/src/pages/common/TopicRegis/index.js
+++ client/src/pages/common/TopicRegis/index.js
@@ -1,5 +1,6 @@
 import Form from "react-bootstrap/Form";
 import Button from "react-bootstrap/Button";
+import Table from "react-bootstrap/Table";
 import { useState } from "react";
 
 import "./style.css";
@@ -10,16 +11,20 @@ import { SearchFaculty, SearchWord } from "../../../components/Shares/Search";
 import useApi from "../../../hooks/useGetApi";
 import Loading from "../../../components/Shares/Loading";
 import PaginationCustom from "../../../components/Shares/Pagination";
+import Action from "../../../components/Shares/Action";
 
 const TopicRegis = () => {
   const [filter, setFilter] = useState({
     facultyId: 0,
     word: "",
-    limit: 10
+    limit: 10,
   });
-  const [pagi, setPagi] = useState(0)
-  let url = `/api/topic-registation?search=${filter.word}&facultyId=${filter.facultyId}&limit=${filter.limit}&offset=${pagi}`;
+  const [pagi, setPagi] = useState(1);
+  let url = `/api/topic-registation?search=${filter.word}&facultyId=${
+    filter.facultyId
+  }&limit=${filter.limit}&offset=${pagi - 1}&status=2`;
   const { data, isLoading, error, fetchData } = useApi(url);
+  console.log(error);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -29,7 +34,6 @@ const TopicRegis = () => {
   var listTopic = [];
 
   if (data && data.total !== 0) {
-    console.log(data.total)
     listTopic = data.topicRegistrations;
   }
 
@@ -52,25 +56,26 @@ const TopicRegis = () => {
           </Form>
         </SubCard>
         <SubCard title="Danh sách">
-          {listTopic.length == 0 ? (
+          {listTopic.length === 0 ? (
             <EmptyListNoti title={"Không có đề tài nào!"} />
           ) : (
             <div>
-              <table className="table">
-                <thead className="table-head">
-                  <tr className="table-tr">
-                    <th>STT</th>
+              <Table bordered hover>
+                <thead>
+                  <tr>
+                    <th>Mã đề xuất</th>
                     <th>Người hướng dẫn</th>
                     <th>Khoa</th>
-                    <th>Số điện thoại liên hệ Email</th>
+                    <th>Số điện thoại - Email</th>
                     <th>Tên đề tài đề xuất</th>
+                    <th>Thao tác</th>
                   </tr>
                 </thead>
                 <tbody>
                   {listTopic.map((item, index) => {
                     return (
                       <tr key={index}>
-                        <td>{index + 1}</td>
+                        <td>{item.id}</td>
                         <td>{item.lecture}</td>
                         <td>{item.faculty}</td>
                         <td>
@@ -79,13 +84,26 @@ const TopicRegis = () => {
                           {item.email}
                         </td>
                         <td>{item.name}</td>
+                        <td>
+                          <Action
+                            todo={[
+                              { name: "Xem chi tiết", href: "#" },
+                              { name: "Đăng ký", href: "#" },
+                            ]}
+                          />
+                        </td>
                       </tr>
                     );
                   })}
                 </tbody>
-              </table>
+              </Table>
 
-              <PaginationCustom setPagi={setPagi} currentPage={pagi} total={data.total} limit={filter.limit}/>
+              <PaginationCustom
+                setPagi={setPagi}
+                currentPage={pagi}
+                total={data.total}
+                limit={filter.limit}
+              />
             </div>
           )}
         </SubCard>
