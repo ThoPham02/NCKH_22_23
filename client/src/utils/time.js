2c882504e80ce2e994d3708f6ed34b1d924cb490
ThoPham02 <2021050819@student.humg.edu.vn>
2023-05-14 00:51:34 +0700

    fix

--- client/src/utils/time.js
+++ client/src/utils/time.js
@@ -1,5 +1,23 @@
-export function CompareTime(time) {
-    const currentTimeUTC0 = new Date().getTime() - new Date().getTimezoneOffset() * 60000;
-    const compareTimeUTC0 = new Date(time).getTime() - new Date(time).getTimezoneOffset() * 60000;
-    return compareTimeUTC0 > currentTimeUTC0;
+export function convertTimestampToDateString(timestamp) {
+  const date = new Date(timestamp);
+
+  const year = date.getFullYear();
+  const month = (date.getMonth() + 1).toString().padStart(2, "0");
+  const day = date.getDate().toString().padStart(2, "0");
+
+  const dateString = `${day}/${month}/${year}`;
+
+  return dateString;
 }
+
+export function convertDateToTimestamp(dateString) {
+    const dateParts = dateString.split('/');
+    const day = parseInt(dateParts[0], 10);
+    const month = parseInt(dateParts[1], 10) - 1;
+    const year = parseInt(dateParts[2], 10);
+  
+    const date = new Date(year, month, day);
+    const timestamp = date.getTime() / 1000; // Chia cho 1000 để chuyển từ miligiây sang giây
+  
+    return timestamp;
+  }
