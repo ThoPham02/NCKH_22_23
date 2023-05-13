a7008932fdd22fb10131518b1490a3a80206e517
ThoPham02 <2021050819@student.humg.edu.vn>
2023-05-13 14:59:00 +0700

    add api create event

--- newserver/service/progress/api/internal/logic/createeventlogic.go
+++ newserver/service/progress/api/internal/logic/createeventlogic.go
@@ -2,9 +2,13 @@ package logic
 
 import (
 	"context"
+	"database/sql"
 
+	"github.com/ThoPham02/research_management/common"
 	"github.com/ThoPham02/research_management/service/progress/api/internal/svc"
 	"github.com/ThoPham02/research_management/service/progress/api/internal/types"
+	"github.com/ThoPham02/research_management/service/progress/model"
+	"github.com/ThoPham02/research_management/sync"
 
 	"github.com/zeromicro/go-zero/core/logx"
 )
@@ -25,6 +29,52 @@ func NewCreateEventLogic(ctx context.Context, svcCtx *svc.ServiceContext) *Creat
 
 func (l *CreateEventLogic) CreateEvent(req *types.CreateEventReq) (resp *types.CreateEventRes, err error) {
 	// todo: add your logic here and delete this line
+	l.Logger.Info("CreateEvent", req)
 
-	return
+	var eventID int64 = sync.RandomID()
+	_, err = l.svcCtx.EventModel.Insert(l.ctx, &model.EventTbl{
+		Id:   eventID,
+		Name: req.Name,
+		SchoolYear: sql.NullString{
+			Valid:  req.SchoolYear != "",
+			String: req.SchoolYear,
+		},
+		IsCurrent: sql.NullInt64{
+			Valid: true,
+			Int64: 0,
+		},
+	})
+	if err != nil {
+		l.Logger.Error(err)
+		return &types.CreateEventRes{
+			Result: types.Result{
+				Code:    common.DB_ERR_CODE,
+				Message: common.DB_ERR_MESS,
+			},
+		}, nil
+	}
+
+	err = l.svcCtx.EventModel.UpdateCurrentEvent(l.ctx, eventID)
+	if err != nil {
+		l.Logger.Error(err)
+		return &types.CreateEventRes{
+			Result: types.Result{
+				Code:    common.DB_ERR_CODE,
+				Message: common.DB_ERR_MESS,
+			},
+		}, nil
+	}
+
+	return &types.CreateEventRes{
+		Result: types.Result{
+			Code:    common.SUCCESS_CODE,
+			Message: common.SUCCESS_MESS,
+		},
+		CurrentEvent: types.Event{
+			ID:         eventID,
+			Name:       req.Name,
+			SchoolYear: req.SchoolYear,
+			IsCurrent:  1,
+		},
+	}, nil
 }
--- newserver/service/progress/api/internal/svc/servicecontext.go
+++ newserver/service/progress/api/internal/svc/servicecontext.go
@@ -2,14 +2,27 @@ package svc
 
 import (
 	"github.com/ThoPham02/research_management/service/progress/api/internal/config"
+	"github.com/ThoPham02/research_management/service/progress/model"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 )
 
 type ServiceContext struct {
-	Config config.Config
+	Config            config.Config
+	EventModel        model.EventTblModel
+	LibraryModel      model.LibraryTblModel
+	NotificationModel model.NotificationTblModel
+	StageModel        model.StageTblModel
+	TopicReportModel  model.TopicReportTblModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewSqlConn(c.Database.Name, c.Database.Source)
 	return &ServiceContext{
-		Config: c,
+		Config:            c,
+		EventModel:        model.NewEventTblModel(conn),
+		LibraryModel:      model.NewLibraryTblModel(conn),
+		NotificationModel: model.NewNotificationTblModel(conn),
+		StageModel:        model.NewStageTblModel(conn),
+		TopicReportModel:  model.NewTopicReportTblModel(conn),
 	}
 }
